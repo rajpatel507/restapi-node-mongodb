2afef9f6b0746763afbb5c6493adc62405b17aa0
Rajesh <rajesh.kathiriya507@gmail.com>
2017-01-05 17:32:12 +0530

    added JWT for authentication and helmet

---
+++ app/routers/authRoute.js
@@ -0,0 +1,68 @@
+//var express = require('express');
+var Cart = require('../models/Cart');
+
+var routes = function(apiRoutes) {
+
+    apiRoutes.route('/')
+        .post(function(req, res) {
+            var cart = new Cart();
+            cart.name = req.body.name;
+            cart.price = req.body.price;
+
+            cart.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Item added to cart' });
+            });
+        })
+        .get(function(req, res) {
+            Cart.find(function(err, carts) {
+                if (err)
+                    res.send(err);
+
+                res.json(carts);
+            });
+        });
+
+    apiRoutes.route('/:item_id')
+        .get(function(req, res) {
+            Cart.findById(req.params.item_id, function(err, Cart) {
+                if (err)
+                    res.send(err);
+                res.json(Cart);
+            });
+        })
+        .put(function(req, res) {
+            Cart.findById(req.params.item_id, function(err, cart) {
+
+                if (err)
+                    res.send(err);
+
+                cart.name = req.body.name;
+                cart.price = req.body.price;
+
+
+                cart.save(function(err) {
+                    if (err)
+                        res.send(err);
+
+                    res.json({ message: 'Cart updated!' });
+                });
+
+            });
+        })
+        .delete(function(req, res) {
+            Cart.remove({
+                _id: req.params.item_id
+            }, function(err, bear) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Successfully deleted' });
+            });
+        });
+    return apiRoutes;
+};
+
+module.exports = routes;
--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var cookieParser = require('cookie-parser');
 var cartRouter = require('./app/routers/cartRoutes');
+var jwt = require('jsonwebtoken');
+//var authRouter = require('./app/routers/authRoutes');
+var User = require('./app/models/User');
+var helmet = require('helmet')
+var apiRoutes = express.Router();
 require('dotenv').config();
 
 var options = {
@@ -25,12 +30,80 @@ conn.once('open', function() {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(helmet());
 
 app.get('/', function(req, res) {
     res.send("Root Working fine");
 });
 
-app.use('/api/carts', cartRouter());
+// app.post('/api/addUser',function(req,res){
+// 	var user = new User({ 
+//     name: 'Rajesh', 
+//     password: 'password',
+//     admin: true 
+//   });
+
+// 	user.save(function(err){
+// 		if (err) throw err;
+
+//     	console.log('User saved successfully');
+//     	res.json({ success: true });
+// 	})
+
+// });
+
+app.post('/api/authenticate', function(req, res) {
+    User.findOne({ name: req.body.name }, function(err, user) {
+        if (err) throw err;
+        if (!user) {
+            res.json({ success: false, message: 'Authentication failed. User not found.' });
+        } else if (user) {
+
+            if (user.password != req.body.password) {
+                res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+            } else {
+                var token = jwt.sign(user, process.env.KEY, {
+                    expiresIn: 18000 // expires in 60*5 minute
+                });
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+            }
+
+        }
+
+    })
+});
+
+
+apiRoutes.use(function(req, res, next) {
+    var token = req.headers['x-access-token'];
+    if (token) {
+
+        jwt.verify(token, process.env.KEY, function(err, decoded) {
+            if (err) {
+                return res.json({ success: false, message: 'Failed to authenticate token.' });
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        });
+
+    } else {
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+
+    }
+});
+
+//Order is important to bypass/api/authenticate from token
+app.use('/api', apiRoutes);
+//app.use('/api/authenticate', userRouter(apiRoutes));
+app.use('/api/carts', cartRouter(apiRoutes));
 
 
 var port = process.env.PORT || 8080;
