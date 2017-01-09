cf05ab903cda79c87fae2c946f5755a445ee1136
Rajesh <rajesh.kathiriya507@gmail.com>
2017-01-09 16:14:54 +0530

    gruntfile change

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp'),
-	nodemon =require('gulp-nodemon');
+	nodemon =require('gulp-nodemon'),
+	jshint = require('gulp-jshint'),
+	sourcemaps = require('gulp-sourcemaps'),
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat');
 
 
 gulp.task('default',function () {
@@ -11,4 +15,21 @@ gulp.task('default',function () {
 	.on('restart',function(){
 		console.log("server restarted")
 	});
-})
+});
+
+
+gulp.task('jshint', function() {
+  return gulp.src('source/app/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('build', function () {
+ return gulp.src('source/app/server.js')
+      //.pipe(sourcemaps.init())
+     // .pipe(concat('bundle.js'))
+      //only uglify if gulp is ran with '--type production'
+      .pipe(uglify()) 
+    //.pipe(sourcemaps.write())
+    .pipe(gulp.dest('build'));
+});
